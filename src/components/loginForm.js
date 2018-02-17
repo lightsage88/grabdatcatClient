ba1eff8961006e411bcf68da3f69382afd2217de
lightsage88 <adrian.e.rosales@gmail.com>
2018-02-16 17:27:42 -0800

    fixed the registration button on the front page

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -56,7 +56,7 @@ export class LoginForm extends React.Component {
 			<input id='password' required minLength='10' type='password' name='password' placeholder='PASSWORD'/>
 			<br/>
 			<Button className='loginFormButtons'>LOGIN</Button>
-			<Link to='/registration'><Button className='loginFormButtons'>REGISTER</Button></Link>
+			<Link to='/registration'><Button onClick={()=>window.location='/registration'}className='loginFormButtons'>REGISTER</Button></Link>
 		</form>
 		</div>
 	</Router>
