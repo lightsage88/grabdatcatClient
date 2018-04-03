fbdcf61630f5e458b387793ff672f6bcb014fda2
lightsage88 <adrian.e.rosales@gmail.com>
2018-04-03 15:49:57 -0700

    deleted a huge block of code that was in direct violation of DRY principles and used a nice ternary operator in landing js instead to display an error message

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,101 +7,56 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 export class Landing extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
 render() {
   let validLogin = this.props.validLogin;
-    if(validLogin === false){
-      return(
-       <div className="App">
-      <section className='topHat'>
-                <span className='notRegistered'>You made a mistake, hooman.</span>
-
-      <div class="main">
-    <span class="stand"></span>
-    <div class="cat">
-     <div class="body"></div>
-     <div class="head">
-       <div class="ear"></div>
-     <div class="ear"></div>
-     </div>
-     <div class="face">
-      <div class="nose"></div>
-     <div class="whisker-container">
-       <div class="whisker"></div>
-       <div class="whisker"></div>
-     </div>
-     <div class="whisker-container">
-       <div class="whisker"></div>
-       <div class="whisker"></div>
-     </div>
-     </div>
-     <div class="tail-container">
-      <div class="tail">
-        <div class="tail">
-          <div class="tail">
-            <div class="tail">
-              <div class="tail">
-                <div class="tail">
-                  <div class="tail"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-     </div>
-   </div>
-</div>
-       
-      </section>
-      <h1 className='funkyTitle'>GRAB DAT CAT</h1>
-      <section className='titleAndBlurb'>
-          <h1 className="App-title">the purrrfect match</h1>
-                    <span>Find a cat to adopt and use Myers-Briggs personality tests to determine which cat breed will suit you best! Adopting a pet can involve rough adjustment periods for beasts both 4 and 2 legged! This app will assist you in adopting a cat that is likely to strike a harmonious chord with you.</span>
-
-
-          
-          
-          <LoginForm />
-       </section>
-        
-          
-      </div>
-      );
-  }
+  console.log(validLogin);
+  
     return (
 
        
       <div className="App">
       <section className='topHat'>
-                <span className='notRegistered'>Not registered? <Link id='registerCaption' to='/registration'>Fix that, hooman.</Link></span>
+      {(validLogin !== false) ? (
+        <span id='catBubbleSpeech' className='notRegistered'>
+                Not registered?<Link id='registerCaption' to='/registration'> Fix that, hooman.</Link>
+        </span>
+                ) : 
+      (<span className='notRegistered'>You made a mistake, hooman.</span>)}
+                
 
-      <div class="main">
-    <span class="stand"></span>
-    <div class="cat">
-     <div class="body"></div>
-     <div class="head">
-       <div class="ear"></div>
-     <div class="ear"></div>
+      <div className="main">
+    <span className="stand"></span>
+    <div className="cat">
+     <div className="body"></div>
+     <div className="head">
+       <div className="ear"></div>
+     <div className="ear"></div>
      </div>
-     <div class="face">
-      <div class="nose"></div>
-     <div class="whisker-container">
-       <div class="whisker"></div>
-       <div class="whisker"></div>
+     <div className="face">
+      <div className="nose"></div>
+     <div className="whisker-container">
+       <div className="whisker"></div>
+       <div className="whisker"></div>
      </div>
-     <div class="whisker-container">
-       <div class="whisker"></div>
-       <div class="whisker"></div>
+     <div className="whisker-container">
+       <div className="whisker"></div>
+       <div className="whisker"></div>
      </div>
      </div>
-     <div class="tail-container">
-      <div class="tail">
-        <div class="tail">
-          <div class="tail">
-            <div class="tail">
-              <div class="tail">
-                <div class="tail">
-                  <div class="tail"></div>
+     <div className="tail-container">
+      <div className="tail">
+        <div className="tail">
+          <div className="tail">
+            <div className="tail">
+              <div className="tail">
+                <div className="tail">
+                  <div className="tail"></div>
                 </div>
               </div>
             </div>
